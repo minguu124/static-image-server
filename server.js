313866cc83855d11453b441fa5efd20208e1835b
minhstartupoi <minh.nguyen@startupoi.com>
2022-07-26 19:42:49 +0700

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, function () {
   console.log("Server is running on PORT", port);
